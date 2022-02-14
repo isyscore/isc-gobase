effe096db5be8b9749278ed9e23bfef5a2dbacad
库陈胜 <xiongm@isyscore.com>
2022-02-14 11:27:36 +0800

    [update] 针对map的Key进行约束

--- isc/listobj.go
+++ isc/listobj.go
@@ -217,7 +217,7 @@ func (l ISCList[T]) DropLastWhile(n int, f func(T) bool) ISCList[T] {
 //AssociateBy Returns a Map containing the elements from the given collection indexed by the key returned from keySelector function applied to each element.
 //If any two elements would have the same key returned by keySelector the last one gets added to the map.
 //The returned map preserves the entry iteration order of the original collection.
-func AssociateBy[T, K any](list []T, keySelector func(T) K) map[K]T {
+func AssociateBy[T any, K comparable](list []T, keySelector func(T) K) map[K]T {
 	r := make(map[K]T)
 	for _, e := range list {
 		r[keySelector(e)] = e
@@ -228,7 +228,7 @@ func AssociateBy[T, K any](list []T, keySelector func(T) K) map[K]T {
 //AssociateByAndValue Returns a Map containing the values provided by valueTransform and indexed by keySelector functions applied to elements of the given collection.
 //If any two elements would have the same key returned by keySelector the last one gets added to the map.
 //The returned map preserves the entry iteration order of the original collection.
-func AssociateByAndValue[T, K, V any](list []T, keySelector func(T) K, valueTransform func(T) V) map[K]V {
+func AssociateByAndValue[T, V any, K comparable](list []T, keySelector func(T) K, valueTransform func(T) V) map[K]V {
 	r := make(map[K]V)
 	for _, e := range list {
 		r[keySelector(e)] = valueTransform(e)
@@ -238,7 +238,7 @@ func AssociateByAndValue[T, K, V any](list []T, keySelector func(T) K, valueTran
 
 // AssociateByTo Populates and returns the destination mutable map with key-value pairs, where key is provided by the keySelector function applied to each element of the given collection and value is the element itself.
 //If any two elements would have the same key returned by keySelector the last one gets added to the map
-func AssociateByTo[T, K any](list []T, keySelector func(T) K, destination map[K]T) map[K]T {
+func AssociateByTo[T, K comparable](list []T, keySelector func(T) K, destination map[K]T) map[K]T {
 	for _, e := range list {
 		destination[keySelector(e)] = e
 	}
@@ -247,7 +247,7 @@ func AssociateByTo[T, K any](list []T, keySelector func(T) K, destination map[K]
 
 // AssociateByAndValueTo Populates and returns the destination mutable map with key-value pairs, where key is provided by the keySelector function applied to each element of the given collection and value is the element itself.
 //If any two elements would have the same key returned by keySelector the last one gets added to the map
-func AssociateByAndValueTo[T, K, V any](list []T, keySelector func(T) K, valueTransform func(T) V, destination map[K]V) map[K]V {
+func AssociateByAndValueTo[T, V any, K comparable](list []T, keySelector func(T) K, valueTransform func(T) V, destination map[K]V) map[K]V {
 	for _, e := range list {
 		destination[keySelector(e)] = valueTransform(e)
 	}
@@ -257,7 +257,7 @@ func AssociateByAndValueTo[T, K, V any](list []T, keySelector func(T) K, valueTr
 //AssociateWith Returns a Map where keys are elements from the given collection and values are produced by the valueSelector function applied to each element.
 //If any two elements are equal, the last one gets added to the map.
 //The returned map preserves the entry iteration order of the original collection.
-func AssociateWith[T, V any](list []T, valueSelector func(T) V) map[T]V {
+func AssociateWith[T comparable, V any](list []T, valueSelector func(T) V) map[T]V {
 	destination := make(map[T]V)
 	for _, e := range list {
 		destination[e] = valueSelector(e)
@@ -267,7 +267,7 @@ func AssociateWith[T, V any](list []T, valueSelector func(T) V) map[T]V {
 
 //AssociateWithTo Populates and returns the destination mutable map with key-value pairs for each element of the given collection, where key is the element itself and value is provided by the valueSelector function applied to that key.
 //If any two elements are equal, the last one overwrites the former value in the map.
-func AssociateWithTo[T, V any](list []T, destination map[T]V, valueSelector func(T) V) map[T]V {
+func AssociateWithTo[T comparable, V any](list []T, destination map[T]V, valueSelector func(T) V) map[T]V {
 	for _, e := range list {
 		destination[e] = valueSelector(e)
 	}
