449eb7c7a98e566edc34bb56d6ee52ad387a407c
库陈胜 <xiongm@isyscore.com>
2022-02-14 12:37:50 +0800

    [update] 单测方法associateBy优化

--- isc/listobj_test.go
+++ isc/listobj_test.go
@@ -11,12 +11,21 @@ type MyStruct struct {
 
 func TestISCList_associateBy(t *testing.T) {
 	var testList ISCList[MyStruct]
-	s1 := &MyStruct{
+	s1 := MyStruct{
 		Name: "K",
 		Age:  1,
 	}
-	testList.Add(*s1)
-	AssociateBy[MyStruct](testList, func(t MyStruct) interface{} {
+	testList.Add(s1)
+	testList.Add(MyStruct{
+		Name: "K2",
+		Age:  2,
+	})
+	testList.Add(MyStruct{
+		Name: "K3",
+		Age:  3,
+	})
+	l := AssociateBy[MyStruct](testList, func(t MyStruct) interface{} {
 		return t.Name
 	})
+	println(ToString(l))
 }
