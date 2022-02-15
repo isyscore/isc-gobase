c11e31ddbe6504d102cd0ed9cd399d50e689754f
kuchensheng <787211647@qq.com>
2022-02-15 23:32:01 +0800

    [update] 添加冰墩墩banner

--- isc/banner.go
+++ isc/banner.go
@@ -46,3 +46,34 @@ var defaultBanner = `
  :: iSysCore Service (GOLANG) ::
 
 `
+
+var BindDwenDwen = `
+        .:          :j        
+         i.,,......:Ei:       
+        KWG        .,#        
+        Wi .:t.  i:, ;,       
+       .. ..jj;;;;fKL .       
+         iD:,     .iLi    .   
+        ,i.         .Df .LWLi 
+      . .: ###    ###L:::KEW  
+      ;,; W#f#W  E:###fiLWKW  
+      .::##t#iL ; K###DttW# : 
+     ..; W###W  # ,,##,if#W.  
+     , :.#### K .j ###tj,#    
+     , i. ##    ..  K#;:;.t   
+    ...,:.      ..  .:f; ;    
+   .#L..,f..   ....:,;,;      
+   K#W..t:;.:...::,,:Li:      
+   W#K:.,t;it;j;,iLjfj;,      
+  EW#D;... :iii,..ji,f,       
+  D##:......,:,;;,:,:,,       
+  :EL  :...        .:,.    :: Bing Dwen Dwen ::   
+   .   .:..    i;  .::     :: iSysCore Service (GOLANG) ::    
+       .::....   ..:,         
+        ::::..:;;;:,.         
+         f::::::j,,;L         
+        ,LLL:,,,;ifG.         
+        fDKEL;.:GWWW.         
+        tWWWK   #WW#i         
+        ,j,j.   t#Kjt
+`
