206f82e0be6fbf54e67ca9087da3d5abbebd5cf6
kuchensheng <787211647@qq.com>
2022-02-17 00:30:05 +0800

    [add] config.go 添加LoadYamlConfig注释

--- config/config.go
+++ config/config.go
@@ -2,7 +2,10 @@ package config
 
 import (
 	"fmt"
+	"github.com/rs/zerolog/log"
 	"io/ioutil"
+	"os"
+	"path/filepath"
 	"reflect"
 
 	"github.com/isyscore/isc-gobase/logger"
@@ -31,33 +34,54 @@ type AppApplication struct {
 	Name string `yaml:"name"`
 }
 
-func LoadConfig(AConfig any) {
-	yamlFile, err := ioutil.ReadFile("./application.yml")
-	if err != nil {
-		logger.Error("读取 application.yml 失败")
-		return
-	}
-	err = yaml.Unmarshal(yamlFile, AConfig)
-	if err != nil {
-		logger.Error("读取 application.yml 异常(%v)", err)
-		return
-	}
-	v1 := reflect.ValueOf(AConfig).Elem()
-	o1 := v1.FieldByName("Spring").Interface()
-	v2 := reflect.ValueOf(o1)
-	o2 := v2.FieldByName("Profiles").Interface()
-	v3 := reflect.ValueOf(o2)
-	act := v3.FieldByName("Active").String()
+//LoadYamlConfig read fileName from private path fileName,eg:application.yml, and transform it to AConfig
+//note: AConfig must be a pointer
+func LoadYamlConfig(fileName string, AConfig any, handler func(data []byte, AConfig any) error) error {
+	pwd, _ := os.Getwd()
+	fp := filepath.Join(pwd, fileName)
+	return LoadYamlConfigByAbsolutPath(fp, AConfig, handler)
+}
 
-	if act != "" && act != "default" {
-		yamlAdditional, err := ioutil.ReadFile(fmt.Sprintf("./application-%s.yml", act))
+//LoadYamlConfigByAbsolutPath read fileName from absolute path fileName,eg:/home/isc-gobase/application.yml, and transform it to AConfig
+//note: AConfig must be a pointer
+func LoadYamlConfigByAbsolutPath(path string, AConfig any, handler func(data []byte, AConfig any) error) error {
+	data, err := ioutil.ReadFile(path)
+	if err != nil {
+		log.Error().Msgf("文件[%s]读取异常\n%v")
+	}
+	return handler(data, AConfig)
+}
+
+//LoadConfig read fileName from current dictionary and fileName is application.yml,eg:/home/isc-gobase/application.yml, and transform it to AConfig
+//note: AConfig must be a pointer
+//note: if it has Spring.Profiles.Active,eg: Spring.Profiles.Active=dev,will load config from /home/isc-gobase/application-dev.yml,and same key
+//will write in the last one.
+func LoadConfig(AConfig any) {
+	LoadYamlConfig("application.yml", AConfig, func(data []byte, AConfig any) error {
+		err := yaml.Unmarshal(data, AConfig)
 		if err != nil {
-			logger.Error("读取 application-%s.yml 失败", act)
-		} else {
-			err = yaml.Unmarshal(yamlAdditional, AConfig)
+			logger.Error("读取 application.yml 异常(%v)", err)
+			return err
+		}
+		v1 := reflect.ValueOf(AConfig).Elem()
+		o1 := v1.FieldByName("Spring").Interface()
+		v2 := reflect.ValueOf(o1)
+		o2 := v2.FieldByName("Profiles").Interface()
+		v3 := reflect.ValueOf(o2)
+		act := v3.FieldByName("Active").String()
+		if act != "" && act != "default" {
+			yamlAdditional, err := ioutil.ReadFile(fmt.Sprintf("./application-%s.yml", act))
 			if err != nil {
-				logger.Error("读取 application-%s.yml 异常", act)
+				logger.Error("读取 application-%s.yml 失败", act)
+				return err
+			} else {
+				err = yaml.Unmarshal(yamlAdditional, AConfig)
+				if err != nil {
+					logger.Error("读取 application-%s.yml 异常", act)
+					return err
+				}
 			}
 		}
-	}
+		return nil
+	})
 }
--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.18
 
 require (
 	github.com/gin-gonic/gin v1.7.7
+	github.com/rs/zerolog v1.26.1
 	gopkg.in/yaml.v2 v2.4.0
 )
 
@@ -19,7 +20,6 @@ require (
 	github.com/modern-go/concurrent v0.0.0-20180228061459-e0a39a4cb421 // indirect
 	github.com/modern-go/reflect2 v0.0.0-20180701023420-4b7aa43c6742 // indirect
 	github.com/ugorji/go/codec v1.1.7 // indirect
-	golang.org/x/crypto v0.0.0-20200622213623-75b288015ac9 // indirect
-	golang.org/x/sys v0.0.0-20210603081109-ebe580a85c40 // indirect
+	golang.org/x/crypto v0.0.0-20211215165025-cf75a172585e // indirect
+	golang.org/x/sys v0.0.0-20210809222454-d867a43fc93e // indirect
 )
-
