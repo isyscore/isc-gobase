8633ad56be2a2b4fe02eac85d72511640dee71f1
酷达舒 <kucs@isyscore.com>
2022-04-02 14:21:47 +0800

    [update] 修改status接口，返回结果中返回version信息，version获取方式：可在application.yml中进行相关配置

--- server/health.go
+++ server/health.go
@@ -2,6 +2,7 @@ package server
 
 import (
 	"fmt"
+	"github.com/isyscore/isc-gobase/config"
 	"net/http"
 	"os"
 	"time"
@@ -13,9 +14,10 @@ import (
 
 var procId = os.Getpid()
 var startTime = time.Now().Format(t2.FmtYMdHms)
+var VERSION *string
 
 func healthSystemStatus(c *gin.Context) {
-	c.Data(http.StatusOK, h2.ContentTypeJson, []byte(fmt.Sprintf(`{"status":"ok","running":true,"pid":%d,"startupAt":"%s"}`, procId, startTime)))
+	c.Data(http.StatusOK, h2.ContentTypeJson, []byte(fmt.Sprintf(`{"status":"ok","running":true,"pid":%d,"startupAt":"%s","version":"%s"}`, procId, startTime, getVersion())))
 }
 
 func healthSystemInit(c *gin.Context) {
@@ -24,4 +26,13 @@ func healthSystemInit(c *gin.Context) {
 
 func healthSystemDestroy(c *gin.Context) {
 	c.Data(http.StatusOK, h2.ContentTypeText, []byte(`{"status":"ok"}`))
+}
+
+func getVersion() string {
+	if VERSION != nil {
+		return *VERSION
+	}
+	version := config.GetValueStringDefault("base.server.version", "unknown")
+	VERSION = &version
+	return *VERSION
 }
