b052a37b9c430360c840009e1e031e922be3b37e
shizi <zhouzy@isyscore.com>
2022-05-17 22:35:36 +0800

    update 放开encoding这个测试

--- go_test.sh
+++ go_test.sh
@@ -5,7 +5,7 @@ go test ./isc/test
 go test ./validate/test
 go test ./compress/test
 go test ./cron/test
-#go test ./encoding/test
+go test ./encoding/test
 go test ./file/test
 go test ./goid/test
 go test ./i18n/test
