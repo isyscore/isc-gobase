99adc598b018aeea7aea021a3ebfd097fffc107b
shizi <zhouzy@isyscore.com>
2022-05-23 19:50:57 +0800

    add 添加拦截-9信号

--- server/server.go
+++ server/server.go
@@ -122,14 +122,15 @@ func StartServer() {
 			logger.Warn("listen: %v\n", err)
 		}
 	}()
+
 	// 等待中断信号来优雅地关闭服务器，为关闭服务器操作设置一个5秒的超时
 	quit := make(chan os.Signal, 1) // 创建一个接收信号的通道
 	// kill 默认会发送 syscall.SIGTERM 信号
 	// kill -2 发送 syscall.SIGINT 信号，我们常用的Ctrl+C就是触发系统SIGINT信号
 	// kill -9 发送 syscall.SIGKILL 信号，但是不能被捕获，所以不需要添加它
 	// signal.Notify把收到的 syscall.SIGINT或syscall.SIGTERM 信号转发给quit
-	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM) // 此处不会阻塞
-	<-quit                                               // 阻塞在此，当接收到上述两种信号时才会往下执行
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM, syscall.SIGKILL) // 此处不会阻塞
+	<-quit                                                                // 阻塞在此，当接收到上述两种信号时才会往下执行
 	logger.Warn("Shutdown Server ...")
 	// 创建一个5秒超时的context
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
