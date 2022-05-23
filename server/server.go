f7019ec5cca437f27c667d8ca0bf1690e7ad20af
shizi <zhouzy@isyscore.com>
2022-05-23 19:54:13 +0800

    add 添加os.kill信号处理

--- server/server.go
+++ server/server.go
@@ -129,8 +129,8 @@ func StartServer() {
 	// kill -2 发送 syscall.SIGINT 信号，我们常用的Ctrl+C就是触发系统SIGINT信号
 	// kill -9 发送 syscall.SIGKILL 信号，但是不能被捕获，所以不需要添加它
 	// signal.Notify把收到的 syscall.SIGINT或syscall.SIGTERM 信号转发给quit
-	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM, syscall.SIGKILL) // 此处不会阻塞
-	<-quit                                                                // 阻塞在此，当接收到上述两种信号时才会往下执行
+	signal.Notify(quit, syscall.SIGINT, os.Kill, syscall.SIGTERM) // 此处不会阻塞
+	<-quit                                                        // 阻塞在此，当接收到上述两种信号时才会往下执行
 	logger.Warn("Shutdown Server ...")
 	// 创建一个5秒超时的context
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
