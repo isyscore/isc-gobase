1254cc1d2ec0896f273da9d0864dc000e74d7765
shizi <zhouzy@isyscore.com>
2022-07-25 15:19:14 +0800

    [update] 临时修改下日志

--- logger/logger.go
+++ logger/logger.go
@@ -219,15 +219,17 @@ func getLogDir(logDir string) string {
 var panicHandler = Strategy{}
 
 func callerFormatter(i interface{}) string {
-	// 去除过多的目录层级信息
+	//去除Jenkins或编译所在主机信息
 	str := i.(string)
-	strs := strings.Split(str, string(os.PathSeparator))
-	ret := strs[len(strs)-1]
+	strs := strings.Split(str, "@2/project")
 	if len(strs) > 1 {
-		ret = strs[len(strs)-2] + string(os.PathSeparator) + ret
+		strs[0] = "../.."
+		str = strings.Join(strs, "")
 	}
-	return ret
+
+	return str
 }
+
 func createFileLeveWriter(level zerolog.Level, strTime string, idx int, dir, appName string) *FileLevelWriter {
 	strL := level.String()
 	if level == zerolog.Disabled {
