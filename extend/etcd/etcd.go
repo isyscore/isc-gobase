bb1dd7f3172eda2ce1607cd897e8cd4e70b79ad3
shizi <lan.zhu.123@163.com>
2022-11-04 16:24:16 +0800

    update 添加etcd的各种api

--- extend/etcd/etcd.go
+++ extend/etcd/etcd.go
@@ -6,6 +6,9 @@ import (
 	"github.com/isyscore/isc-gobase/logger"
 	"github.com/isyscore/isc-gobase/tracing"
 	etcdClientV3 "go.etcd.io/etcd/client/v3"
+	"go.uber.org/zap"
+	"google.golang.org/grpc"
+	"io"
 	"time"
 )
 
@@ -142,6 +145,10 @@ func (etcdWrap *EtcdClientWrap) Delete(ctx context.Context, key string, opts ...
 	return rsp, err
 }
 
+func (etcdWrap *EtcdClientWrap) Compact(ctx context.Context, rev int64, opts ...etcdClientV3.CompactOption) (*etcdClientV3.CompactResponse, error) {
+	return etcdWrap.Client.Compact(ctx, rev, opts...)
+}
+
 func (etcdWrap *EtcdClientWrap) Do(ctx context.Context, op etcdClientV3.Op) (etcdClientV3.OpResponse, error) {
 	if !EtcdTracingIsOpen() {
 		return etcdWrap.Client.Do(ctx, op)
@@ -150,6 +157,198 @@ func (etcdWrap *EtcdClientWrap) Do(ctx context.Context, op etcdClientV3.Op) (etc
 	rsp, err := etcdWrap.Client.Do(ctx, op)
 	etcdWrap.etcdHook.After(ctx, op, rsp, err)
 	return rsp, err
+}
+
+func (etcdWrap *EtcdClientWrap) Txn(ctx context.Context) etcdClientV3.Txn {
+	return etcdWrap.Client.Txn(ctx)
+}
+
+func (etcdWrap *EtcdClientWrap) MemberList(ctx context.Context) (*etcdClientV3.MemberListResponse, error) {
+	return etcdWrap.Client.MemberList(ctx)
+}
+
+func (etcdWrap *EtcdClientWrap) MemberAdd(ctx context.Context, peerAddrs []string) (*etcdClientV3.MemberAddResponse, error) {
+	return etcdWrap.Client.MemberAdd(ctx, peerAddrs)
+}
+
+func (etcdWrap *EtcdClientWrap) MemberAddAsLearner(ctx context.Context, peerAddrs []string) (*etcdClientV3.MemberAddResponse, error) {
+	return etcdWrap.Client.MemberAddAsLearner(ctx, peerAddrs)
+}
+
+func (etcdWrap *EtcdClientWrap) MemberRemove(ctx context.Context, id uint64) (*etcdClientV3.MemberRemoveResponse, error) {
+	return etcdWrap.Client.MemberRemove(ctx, id)
+}
+
+func (etcdWrap *EtcdClientWrap) MemberPromote(ctx context.Context, id uint64) (*etcdClientV3.MemberPromoteResponse, error) {
+	return etcdWrap.Client.MemberPromote(ctx, id)
+}
+
+func (etcdWrap *EtcdClientWrap) Grant(ctx context.Context, ttl int64) (*etcdClientV3.LeaseGrantResponse, error) {
+	return etcdWrap.Client.Grant(ctx, ttl)
+}
+
+func (etcdWrap *EtcdClientWrap) Revoke(ctx context.Context, id etcdClientV3.LeaseID) (*etcdClientV3.LeaseRevokeResponse, error) {
+	return etcdWrap.Client.Revoke(ctx, id)
+}
+
+func (etcdWrap *EtcdClientWrap) TimeToLive(ctx context.Context, id etcdClientV3.LeaseID, opts ...etcdClientV3.LeaseOption) (*etcdClientV3.LeaseTimeToLiveResponse, error) {
+	return etcdWrap.Client.TimeToLive(ctx, id, opts...)
+}
+
+func (etcdWrap *EtcdClientWrap) Leases(ctx context.Context) (*etcdClientV3.LeaseLeasesResponse, error) {
+	return etcdWrap.Client.Leases(ctx)
+}
+
+func (etcdWrap *EtcdClientWrap) KeepAlive(ctx context.Context, id etcdClientV3.LeaseID) (<-chan *etcdClientV3.LeaseKeepAliveResponse, error) {
+	return etcdWrap.Client.KeepAlive(ctx, id)
+}
+
+func (etcdWrap *EtcdClientWrap) KeepAliveOnce(ctx context.Context, id etcdClientV3.LeaseID) (*etcdClientV3.LeaseKeepAliveResponse, error) {
+	return etcdWrap.Client.KeepAliveOnce(ctx, id)
+}
+
+func (etcdWrap *EtcdClientWrap) Close() error {
+	return etcdWrap.Client.Close()
+}
+
+func (etcdWrap *EtcdClientWrap) Watch(ctx context.Context, key string, opts ...etcdClientV3.OpOption) etcdClientV3.WatchChan {
+	return etcdWrap.Client.Watch(ctx, key, opts...)
+}
+
+func (etcdWrap *EtcdClientWrap) RequestProgress(ctx context.Context) error {
+	return etcdWrap.Client.RequestProgress(ctx)
+}
+
+func (etcdWrap *EtcdClientWrap)Authenticate(ctx context.Context, name string, password string) (*etcdClientV3.AuthenticateResponse, error) {
+	return etcdWrap.Client.Authenticate(ctx, name, password)
+}
+
+func (etcdWrap *EtcdClientWrap) AuthEnable(ctx context.Context) (*etcdClientV3.AuthEnableResponse, error) {
+	return etcdWrap.Client.AuthEnable(ctx)
+}
+
+func (etcdWrap *EtcdClientWrap) AuthDisable(ctx context.Context) (*etcdClientV3.AuthDisableResponse, error) {
+	return etcdWrap.Client.AuthDisable(ctx)
+}
+
+func (etcdWrap *EtcdClientWrap) AuthStatus(ctx context.Context) (*etcdClientV3.AuthStatusResponse, error) {
+	return etcdWrap.Client.AuthStatus(ctx)
+}
+
+func (etcdWrap *EtcdClientWrap) UserAdd(ctx context.Context, name string, password string) (*etcdClientV3.AuthUserAddResponse, error) {
+	return etcdWrap.Client.UserAdd(ctx, name, password)
+}
+
+func (etcdWrap *EtcdClientWrap) UserAddWithOptions(ctx context.Context, name string, password string, opt *etcdClientV3.UserAddOptions) (*etcdClientV3.AuthUserAddResponse, error) {
+	return etcdWrap.Client.UserAddWithOptions(ctx, name, password, opt)
+}
+
+func (etcdWrap *EtcdClientWrap) UserDelete(ctx context.Context, name string) (*etcdClientV3.AuthUserDeleteResponse, error) {
+	return etcdWrap.Client.UserDelete(ctx, name)
+}
+
+func (etcdWrap *EtcdClientWrap) UserChangePassword(ctx context.Context, name string, password string) (*etcdClientV3.AuthUserChangePasswordResponse, error) {
+	return etcdWrap.Client.UserChangePassword(ctx, name, password)
+}
+
+func (etcdWrap *EtcdClientWrap) UserGrantRole(ctx context.Context, user string, role string) (*etcdClientV3.AuthUserGrantRoleResponse, error) {
+	return etcdWrap.Client.UserGrantRole(ctx, user, role)
+}
+
+func (etcdWrap *EtcdClientWrap) UserGet(ctx context.Context, name string) (*etcdClientV3.AuthUserGetResponse, error) {
+	return etcdWrap.Client.UserGet(ctx, name)
+}
+
+func (etcdWrap *EtcdClientWrap) UserList(ctx context.Context) (*etcdClientV3.AuthUserListResponse, error) {
+	return etcdWrap.Client.UserList(ctx)
+}
+
+func (etcdWrap *EtcdClientWrap) UserRevokeRole(ctx context.Context, name string, role string) (*etcdClientV3.AuthUserRevokeRoleResponse, error) {
+	return etcdWrap.Client.UserRevokeRole(ctx, name, role)
+}
+
+func (etcdWrap *EtcdClientWrap) RoleAdd(ctx context.Context, name string) (*etcdClientV3.AuthRoleAddResponse, error) {
+	return etcdWrap.Client.RoleAdd(ctx, name)
+}
+
+func (etcdWrap *EtcdClientWrap) RoleGrantPermission(ctx context.Context, name string, key, rangeEnd string, permType etcdClientV3.PermissionType) (*etcdClientV3.AuthRoleGrantPermissionResponse, error) {
+	return etcdWrap.Client.RoleGrantPermission(ctx, name, key, rangeEnd, permType)
+}
+
+func (etcdWrap *EtcdClientWrap) RoleGet(ctx context.Context, role string) (*etcdClientV3.AuthRoleGetResponse, error) {
+	return etcdWrap.Client.RoleGet(ctx, role)
+}
+
+func (etcdWrap *EtcdClientWrap) RoleList(ctx context.Context) (*etcdClientV3.AuthRoleListResponse, error) {
+	return etcdWrap.Client.RoleList(ctx)
+}
+
+func (etcdWrap *EtcdClientWrap) RoleRevokePermission(ctx context.Context, role string, key, rangeEnd string) (*etcdClientV3.AuthRoleRevokePermissionResponse, error) {
+	return etcdWrap.Client.RoleRevokePermission(ctx, role,key, rangeEnd)
+}
+
+func (etcdWrap *EtcdClientWrap) RoleDelete(ctx context.Context, role string) (*etcdClientV3.AuthRoleDeleteResponse, error) {
+	return etcdWrap.Client.RoleDelete(ctx, role)
+}
+
+func (etcdWrap *EtcdClientWrap) AlarmList(ctx context.Context) (*etcdClientV3.AlarmResponse, error) {
+	return etcdWrap.Client.AlarmList(ctx)
+}
+
+func (etcdWrap *EtcdClientWrap) AlarmDisarm(ctx context.Context, m *etcdClientV3.AlarmMember) (*etcdClientV3.AlarmResponse, error) {
+	return etcdWrap.Client.AlarmDisarm(ctx, m)
+}
+
+func (etcdWrap *EtcdClientWrap) Defragment(ctx context.Context, endpoint string) (*etcdClientV3.DefragmentResponse, error) {
+	return etcdWrap.Client.Defragment(ctx, endpoint)
+}
+
+func (etcdWrap *EtcdClientWrap) Status(ctx context.Context, endpoint string) (*etcdClientV3.StatusResponse, error) {
+	return etcdWrap.Client.Status(ctx, endpoint)
+}
+
+func (etcdWrap *EtcdClientWrap) HashKV(ctx context.Context, endpoint string, rev int64) (*etcdClientV3.HashKVResponse, error) {
+	return etcdWrap.Client.HashKV(ctx, endpoint, rev)
+}
+
+func (etcdWrap *EtcdClientWrap) Snapshot(ctx context.Context) (io.ReadCloser, error) {
+	return etcdWrap.Client.Snapshot(ctx)
+}
+
+func (etcdWrap *EtcdClientWrap) MoveLeader(ctx context.Context, transfereeID uint64) (*etcdClientV3.MoveLeaderResponse, error) {
+	return etcdWrap.Client.MoveLeader(ctx, transfereeID)
+}
+
+func (etcdWrap *EtcdClientWrap) WithLogger(lg *zap.Logger) *etcdClientV3.Client {
+	return etcdWrap.Client.WithLogger(lg)
+}
+
+func (etcdWrap *EtcdClientWrap) GetLogger() *zap.Logger {
+	return etcdWrap.Client.GetLogger()
+}
+
+func (etcdWrap *EtcdClientWrap) Ctx() context.Context {
+	return etcdWrap.Client.Ctx()
+}
+
+func (etcdWrap *EtcdClientWrap) Endpoints() []string {
+	return etcdWrap.Client.Endpoints()
+}
+
+func (etcdWrap *EtcdClientWrap) SetEndpoints(eps ...string) {
+	etcdWrap.Client.SetEndpoints(eps...)
+}
+
+func (etcdWrap *EtcdClientWrap) Sync(ctx context.Context) error {
+	return etcdWrap.Client.Sync(ctx)
+}
+
+func (etcdWrap *EtcdClientWrap) Dial(ep string) (*grpc.ClientConn, error) {
+	return etcdWrap.Client.Dial(ep)
+}
+
+func (etcdWrap *EtcdClientWrap) ActiveConnection() *grpc.ClientConn {
+	return etcdWrap.Client.ActiveConnection()
 }
 
 func EtcdTracingIsOpen() bool {
