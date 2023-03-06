bdb5f0bee15773f468b3235a6e6bc588bf5d06b7
shizi <lan.zhu.123@163.com>
2023-03-06 11:19:17 +0800

    fix 修复redis的默认值不生效问题

--- extend/redis/go-redis.go
+++ extend/redis/go-redis.go
@@ -86,6 +86,74 @@ func getStandaloneConfig() *goredis.Options {
 		IdleTimeout:        baseTime.NumToTimeDuration(config.RedisCfg.IdleTimeout, time.Millisecond),
 		IdleCheckFrequency: baseTime.NumToTimeDuration(config.RedisCfg.IdleCheckFrequency, time.Millisecond),
 	}
+
+	// -------- 命令执行失败配置 --------
+	if config.GetValueString("base.redis.max-retries") == "" {
+		// # 命令执行失败时候，最大重试次数，默认3次，-1（不是0）则不重试
+		redisConfig.MaxRetries = 3
+	}
+
+	if config.GetValueString("base.redis.min-retry-backoff") == "" {
+		// #（单位毫秒） 命令执行失败时候，每次重试的最小回退时间，默认8毫秒，-1则禁止回退
+		redisConfig.MinRetryBackoff = 8*time.Millisecond
+	}
+
+	if config.GetValueString("base.redis.max-retry-backoff") == "" {
+		// #（单位毫秒） 命令执行失败时候，每次重试的最小回退时间，默认8毫秒，-1则禁止回退
+		redisConfig.MinRetryBackoff = 512*time.Millisecond
+	}
+
+	// -------- 超时配置 --------
+	if config.GetValueString("base.redis.dial-timeout") == "" {
+		// # （单位毫秒）超时：创建新链接的拨号超时时间，默认15秒
+		redisConfig.DialTimeout = 15*time.Second
+	}
+
+	if config.GetValueString("base.redis.read-timeout") == "" {
+		// # （单位毫秒）超时：读超时，默认3秒，使用-1，使用-1则表示无超时，0的话是表示默认3秒
+		redisConfig.ReadTimeout = 3*time.Second
+	}
+
+	if config.GetValueString("base.redis.write-timeout") == "" {
+		// # （单位毫秒）超时：写超时，默认是读超时3秒，使用-1，使用-1则表示无超时，0的话是表示默认3秒
+		redisConfig.WriteTimeout = 3*time.Second
+	}
+
+	// -------- 连接池相关配置 --------
+	if config.GetValueString("base.redis.pool-fifo") == "" {
+		// # 连接池类型：fifo：true;lifo：false;和lifo相比，fifo开销更高
+		redisConfig.PoolFIFO = false
+	}
+
+	if config.GetValueString("base.redis.pool-size") == "" {
+		// # 最大连接池大小：默认每个cpu核是10个连接，cpu核数可以根据函数runtime.GOMAXPROCS来配置，默认是runtime.NumCpu
+		redisConfig.PoolSize = 10
+	}
+
+	if config.GetValueString("base.redis.min-idle-conns") == "" {
+		// # 最小空闲连接数
+		redisConfig.MinIdleConns = 10
+	}
+
+	if config.GetValueString("base.redis.max-conn-age") == "" {
+		// #（单位毫秒） 连接存活时长，默认不关闭
+		redisConfig.MaxConnAge = 12*30*24*time.Hour
+	}
+
+	if config.GetValueString("base.redis.pool-timeout") == "" {
+		// #（单位毫秒）获取链接池中的链接都在忙，则等待对应的时间，默认读超时+1秒
+		redisConfig.PoolTimeout = time.Second
+	}
+
+	if config.GetValueString("base.redis.idle-timeout") == "" {
+		// #（单位毫秒）空闲链接时间，超时则关闭，注意：该时间要小于服务端的超时时间，否则会出现拿到的链接失效问题，默认5分钟，-1表示禁用超时检查
+		redisConfig.IdleTimeout = 5 * time.Minute
+	}
+
+	if config.GetValueString("base.redis.idle-check-frequency") == "" {
+		// #（单位毫秒）空闲链接核查频率，默认1分钟。-1禁止空闲链接核查，即使配置了IdleTime也不行
+		redisConfig.IdleCheckFrequency = time.Minute
+	}
 	return redisConfig
 }
 
@@ -117,6 +185,73 @@ func getSentinelConfig() *goredis.FailoverOptions {
 		IdleCheckFrequency: baseTime.NumToTimeDuration(config.RedisCfg.IdleCheckFrequency, time.Millisecond),
 	}
 
+	// -------- 命令执行失败配置 --------
+	if config.GetValueString("base.redis.max-retries") == "" {
+		// # 命令执行失败时候，最大重试次数，默认3次，-1（不是0）则不重试
+		redisConfig.MaxRetries = 3
+	}
+
+	if config.GetValueString("base.redis.min-retry-backoff") == "" {
+		// #（单位毫秒） 命令执行失败时候，每次重试的最小回退时间，默认8毫秒，-1则禁止回退
+		redisConfig.MinRetryBackoff = 8*time.Millisecond
+	}
+
+	if config.GetValueString("base.redis.max-retry-backoff") == "" {
+		// #（单位毫秒） 命令执行失败时候，每次重试的最小回退时间，默认8毫秒，-1则禁止回退
+		redisConfig.MinRetryBackoff = 512*time.Millisecond
+	}
+
+	// -------- 超时配置 --------
+	if config.GetValueString("base.redis.dial-timeout") == "" {
+		// # （单位毫秒）超时：创建新链接的拨号超时时间，默认15秒
+		redisConfig.DialTimeout = 15*time.Second
+	}
+
+	if config.GetValueString("base.redis.read-timeout") == "" {
+		// # （单位毫秒）超时：读超时，默认3秒，使用-1，使用-1则表示无超时，0的话是表示默认3秒
+		redisConfig.ReadTimeout = 3*time.Second
+	}
+
+	if config.GetValueString("base.redis.write-timeout") == "" {
+		// # （单位毫秒）超时：写超时，默认是读超时3秒，使用-1，使用-1则表示无超时，0的话是表示默认3秒
+		redisConfig.WriteTimeout = 3*time.Second
+	}
+
+	// -------- 连接池相关配置 --------
+	if config.GetValueString("base.redis.pool-fifo") == "" {
+		// # 连接池类型：fifo：true;lifo：false;和lifo相比，fifo开销更高
+		redisConfig.PoolFIFO = false
+	}
+
+	if config.GetValueString("base.redis.pool-size") == "" {
+		// # 最大连接池大小：默认每个cpu核是10个连接，cpu核数可以根据函数runtime.GOMAXPROCS来配置，默认是runtime.NumCpu
+		redisConfig.PoolSize = 10
+	}
+
+	if config.GetValueString("base.redis.min-idle-conns") == "" {
+		// # 最小空闲连接数
+		redisConfig.MinIdleConns = 10
+	}
+
+	if config.GetValueString("base.redis.max-conn-age") == "" {
+		// #（单位毫秒） 连接存活时长，默认不关闭
+		redisConfig.MaxConnAge = 12*30*24*time.Hour
+	}
+
+	if config.GetValueString("base.redis.pool-timeout") == "" {
+		// #（单位毫秒）获取链接池中的链接都在忙，则等待对应的时间，默认读超时+1秒
+		redisConfig.PoolTimeout = time.Second
+	}
+
+	if config.GetValueString("base.redis.idle-timeout") == "" {
+		// #（单位毫秒）空闲链接时间，超时则关闭，注意：该时间要小于服务端的超时时间，否则会出现拿到的链接失效问题，默认5分钟，-1表示禁用超时检查
+		redisConfig.IdleTimeout = 5 * time.Minute
+	}
+
+	if config.GetValueString("base.redis.idle-check-frequency") == "" {
+		// #（单位毫秒）空闲链接核查频率，默认1分钟。-1禁止空闲链接核查，即使配置了IdleTime也不行
+		redisConfig.IdleCheckFrequency = time.Minute
+	}
 	return redisConfig
 }
 
@@ -151,6 +286,74 @@ func getClusterConfig() *goredis.ClusterOptions {
 		PoolTimeout:        baseTime.NumToTimeDuration(config.RedisCfg.PoolTimeout, time.Millisecond),
 		IdleTimeout:        baseTime.NumToTimeDuration(config.RedisCfg.IdleTimeout, time.Millisecond),
 		IdleCheckFrequency: baseTime.NumToTimeDuration(config.RedisCfg.IdleCheckFrequency, time.Millisecond),
+	}
+
+	// -------- 命令执行失败配置 --------
+	if config.GetValueString("base.redis.max-retries") == "" {
+		// # 命令执行失败时候，最大重试次数，默认3次，-1（不是0）则不重试
+		redisConfig.MaxRetries = 3
+	}
+
+	if config.GetValueString("base.redis.min-retry-backoff") == "" {
+		// #（单位毫秒） 命令执行失败时候，每次重试的最小回退时间，默认8毫秒，-1则禁止回退
+		redisConfig.MinRetryBackoff = 8*time.Millisecond
+	}
+
+	if config.GetValueString("base.redis.max-retry-backoff") == "" {
+		// #（单位毫秒） 命令执行失败时候，每次重试的最小回退时间，默认8毫秒，-1则禁止回退
+		redisConfig.MinRetryBackoff = 512*time.Millisecond
+	}
+
+	// -------- 超时配置 --------
+	if config.GetValueString("base.redis.dial-timeout") == "" {
+		// # （单位毫秒）超时：创建新链接的拨号超时时间，默认15秒
+		redisConfig.DialTimeout = 15*time.Second
+	}
+
+	if config.GetValueString("base.redis.read-timeout") == "" {
+		// # （单位毫秒）超时：读超时，默认3秒，使用-1，使用-1则表示无超时，0的话是表示默认3秒
+		redisConfig.ReadTimeout = 3*time.Second
+	}
+
+	if config.GetValueString("base.redis.write-timeout") == "" {
+		// # （单位毫秒）超时：写超时，默认是读超时3秒，使用-1，使用-1则表示无超时，0的话是表示默认3秒
+		redisConfig.WriteTimeout = 3*time.Second
+	}
+
+	// -------- 连接池相关配置 --------
+	if config.GetValueString("base.redis.pool-fifo") == "" {
+		// # 连接池类型：fifo：true;lifo：false;和lifo相比，fifo开销更高
+		redisConfig.PoolFIFO = false
+	}
+
+	if config.GetValueString("base.redis.pool-size") == "" {
+		// # 最大连接池大小：默认每个cpu核是10个连接，cpu核数可以根据函数runtime.GOMAXPROCS来配置，默认是runtime.NumCpu
+		redisConfig.PoolSize = 10
+	}
+
+	if config.GetValueString("base.redis.min-idle-conns") == "" {
+		// # 最小空闲连接数
+		redisConfig.MinIdleConns = 10
+	}
+
+	if config.GetValueString("base.redis.max-conn-age") == "" {
+		// #（单位毫秒） 连接存活时长，默认不关闭
+		redisConfig.MaxConnAge = 12*30*24*time.Hour
+	}
+
+	if config.GetValueString("base.redis.pool-timeout") == "" {
+		// #（单位毫秒）获取链接池中的链接都在忙，则等待对应的时间，默认读超时+1秒
+		redisConfig.PoolTimeout = time.Second
+	}
+
+	if config.GetValueString("base.redis.idle-timeout") == "" {
+		// #（单位毫秒）空闲链接时间，超时则关闭，注意：该时间要小于服务端的超时时间，否则会出现拿到的链接失效问题，默认5分钟，-1表示禁用超时检查
+		redisConfig.IdleTimeout = 5 * time.Minute
+	}
+
+	if config.GetValueString("base.redis.idle-check-frequency") == "" {
+		// #（单位毫秒）空闲链接核查频率，默认1分钟。-1禁止空闲链接核查，即使配置了IdleTime也不行
+		redisConfig.IdleCheckFrequency = time.Minute
 	}
 	return redisConfig
 }
